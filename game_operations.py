0076a89e7d97e3f3bfac1eef4074f1b562d6f004
BlueAxolotLArseniy <131585010+BlueAxolotLArseniy@users.noreply.github.com>
2025-02-03 17:45:16 +0300

    fix code and add game_operations

---
+++ game_operations.py
@@ -0,0 +1,54 @@
+import player, consts, pygame
+
+class Game:
+    def __init__(self, sc, player: player.Player, enemies: list, caves: list, camera, game_state):
+        self.sc = sc
+        self.player = player
+        self.enemies = enemies
+        self.caves = caves
+        self.camera = camera
+        self.game_state = game_state
+        
+    def events(self, event):
+        if event.type == pygame.QUIT:
+            exit()
+
+        if event.type == pygame.KEYDOWN:
+
+            if event.key == pygame.K_ESCAPE:
+                self.game_state.toggle_pause()
+
+    def update(self, event):
+        self.sc.fill((0, 0, 0))
+        
+        if self.game_state.active_screen:
+            self.game_state.active_screen.update(event)
+        
+        if not self.game_state.is_paused:
+            self.camera.update()
+            self.player.update(self.camera.kx, self.camera.ky)
+            for cave in self.caves:
+                cave.update(self.camera.kx, self.camera.ky)
+
+            if consts.ENABLE_ENEMIES:
+                for enemy in self.enemies:
+                    enemy.update(self.camera.kx, self.camera.ky)
+
+            if self.game_state.debug_mode:
+                print('Debug --> Player time(ticks): ' + str(self.player.time))
+                print('Debug --> Number of bullets: ' + str(len(self.player.bullets)))
+                print('Debug --> FPS: ' + str(consts.FPS))
+    
+    def draw(self):
+        self.player.draw(self.sc)
+
+        if consts.ENABLE_ENEMIES:
+            for enemy in self.enemies:
+                enemy.draw(self.sc)
+
+        for cave in self.caves:
+            cave.draw(self.sc)
+
+        if self.game_state.active_screen:
+            self.game_state.active_screen.draw(self.sc)
+
--- main.py
+++ main.py
@@ -5,72 +5,37 @@ from enemy import Enemy
 from camera import Camera
 from cave import Cave
 from state import GameState
+from game_operations import Game
 
 pygame.init()
 
 game_state = GameState()
 
-# Main variables   Главные переменные
 sc = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 player = Player(400, 250, game_state)
 enemy = Enemy(500, 250, player, 'WithOneBarrel', game_state)
 enemy2 = Enemy(600, 250, player, 'WithTwoBarrels', game_state)
 cave = Cave(400, 350, game_state)
 
-clock = pygame.time.Clock()  # Creating a Clock   Создание Clock
+clock = pygame.time.Clock()
 
 camera = Camera()
 
+game_operations = Game(sc, 
+                 player, 
+                 [enemy, enemy2], 
+                 [cave], 
+                 camera, 
+                 game_state
+                 )
 
-while 1:  # Main cycle   Главный цикл
-
+while 1:
     for event in pygame.event.get():
 
-        # Ending a programme on exit   Завершение программы при выходе
-        if event.type == pygame.QUIT:
-            exit()
+        game_operations.events(event)
 
-        # All conditions with buttons pressed   Все условия с нажатыми кнопками:
-        if event.type == pygame.KEYDOWN:
-
-            if event.key == pygame.K_ESCAPE:
-                game_state.toggle_pause()
-
-    sc.fill((0, 0, 0))  # Filling with black colour   Заливка черным цветом
-
-    # Updates   Обновления
-
-    if game_state.active_screen:
-        game_state.active_screen.update(event)
-
-    if not game_state.is_paused:
-        camera.update()
-        player.update(camera.kx, camera.ky)
-        cave.update(camera.kx, camera.ky)
-
-        if ENABLE_ENEMIES:
-            enemy.update(camera.kx, camera.ky)
-            enemy2.update(camera.kx, camera.ky)
-
-        if game_state.debug_mode:
-            # The number of ticks from enemy   Количество тиков во враге
-            print('Debug --> Player time(ticks): ' + str(player.time))
-            print('Debug --> Number of bullets: ' + str(len(player.bullets)))
-            print('Debug --> FPS: ' + str(FPS))
-
-    # Draws   Отрисовки
-
-    player.draw(sc)
-
-    if ENABLE_ENEMIES:
-        enemy.draw(sc)
-        enemy2.draw(sc)
-
-    cave.draw(sc)
-
-    if game_state.active_screen:
-        game_state.active_screen.draw(sc)
-
-    clock.tick(FPS)  # Updates ticks   Обновления тиков
-
-    pygame.display.update()  # Update display   Обновление дисплея
+    game_operations.update(event)
+    game_operations.draw()
+    
+    clock.tick(FPS)
+    pygame.display.update()
